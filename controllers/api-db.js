407b5214fbfd442738f4bac69c96acc54b0835ca
Franklin Chieze <chieze.franklin@gmail.com>
2017-07-04 14:21:12 +0100

    Add support for query strings in the URL

--- controllers/api-db.js
+++ controllers/api-db.js
@@ -39,36 +39,36 @@ var __dbOp = function(options, callback){
 				}
 				else if (options.operation == "find") {
 					var collection = db.collection(collectionFullname);
-					collection.find(options.object, options.map, function(err, docs){
+					collection.find(options.query, options.map, function(err, docs){
 						docs.toArray(callback);
 						db.close();
 					});
 				}
 				else if (options.operation == "findone") {
 					var collection = db.collection(collectionFullname);
-					collection.findOne(options.object, options.map, function(err, doc){
+					collection.findOne(options.query, options.map, function(err, doc){
 						callback(err, doc);
 						db.close();
 					});
 				}
 				else if (options.operation == "insert") {
 					var collection = db.collection(collectionFullname);
-					collection.insert(options.object, function(err, doc){
-						callback(err, doc);
+					collection.insert(options.object, function(err, docs){
+						callback(err, docs);
 						db.close();
 					});
 				}
 				else if (options.operation == "remove") {
 					var collection = db.collection(collectionFullname);
 					/*
-					collection.find(options.object, function(err, docs){
+					collection.find(options.query, function(err, docs){
 						//store the array in a temp array before calling 'remove' cuz 'remove' will clear 'docs'
 						var tempArray = [];
 						function tempCallback(tempErr, tempDocs) {
 							tempArray = tempDocs;
 						}
 						docs.toArray(tempCallback);
-						collection.remove(options.object, function(err2, result){
+						collection.remove(options.query, function(err2, result){
 							callback(err2, tempArray);
 							//originally the above line was: docs.toArray(callback);
 							//but I noticed the array was always empty (the call to 'remove' was clearing the array produced by 'find')
@@ -78,14 +78,14 @@ var __dbOp = function(options, callback){
 					});
 					*/
 
-					collection.remove(options.object, function(err, result){
-						callback(err, result.result);
+					collection.remove(options.query, function(err, result){
+						callback(err, result);
 						db.close();
 					});
 				}
 				else if (options.operation == "update") {
 					var collection = db.collection(collectionFullname);
-					collection.update(options.object, options.values, options.options, function(err, doc){
+					collection.update(options.query, options.values, options.options, function(err, doc){
 						callback(err, doc);
 						db.close();
 					});
@@ -100,41 +100,39 @@ var __dbOp = function(options, callback){
 
 module.exports = {
 	delete: function(request, response){
-		utils.Events.fire('app-db-dropping', { body: { app: request.db, db: request.db }, subscribers: [request.db] }, request.appToken, function(eventError, eventResponse){});
-		setTimeout(function(){
-			__dbOp({ db: request.db, operation: "dropdb" }, 
-			function(err, result){
-				utils.Events.fire('app-db-dropped', { body: { app: request.db, db: request.db } }, request.appToken, function(eventError, eventResponse){});
-				response.send(utils.Misc.createResponse(result, err));
-			});
-		}, 2000);
+		__dbOp({ db: request.db, operation: "dropdb" }, 
+		function(err, result){
+			response.send(utils.Misc.createResponse(result, err));
+		});
 	},
 	deleteCollection: function(request, response){
 		__dbOp({ db: request.db, collection: request.params.collection, operation: "drop" }, 
-		function(err, docs){
-			response.send(utils.Misc.createResponse(docs, err));
+		function(err, result){
+			response.send(utils.Misc.createResponse(result, err));
 		});
 	},
 	postCollectionFind: function(request, response){
-		if(utils.Misc.isNullOrUndefined(request.body.object)) {
+		if(utils.Misc.isNullOrUndefined(request.body.query) && utils.Misc.isNullOrUndefined(request.body.object)) {
 			var error = new Error(errors['710']);
 			response.end(utils.Misc.createResponse(null, error, 710));
 			return;
 		}
 
-		__dbOp({ db: request.db, collection: request.params.collection, operation: "find", object: request.body.object, map: request.body.map || {} }, 
+		__dbOp({ db: request.db, collection: request.params.collection, operation: "find", query: request.body.query || request.body.object || request.query, 
+			map: request.body.map || request.body.projection || {} }, 
 		function(err, docs){
 			response.send(utils.Misc.createResponse(docs, err));
 		});
 	},
 	postCollectionFindOne: function(request, response){
-		if(utils.Misc.isNullOrUndefined(request.body.object)) {
+		if(utils.Misc.isNullOrUndefined(request.body.query) && utils.Misc.isNullOrUndefined(request.body.object)) {
 			var error = new Error(errors['710']);
 			response.end(utils.Misc.createResponse(null, error, 710));
 			return;
 		}
 
-		__dbOp({ db: request.db, collection: request.params.collection, operation: "findone", object: request.body.object, map: request.body.map || {} }, 
+		__dbOp({ db: request.db, collection: request.params.collection, operation: "findone", query: request.body.query || request.body.object || request.query, 
+			map: request.body.map || request.body.projection || {} }, 
 		function(err, doc){
 			response.send(utils.Misc.createResponse(doc, err));
 		});
@@ -152,38 +150,38 @@ module.exports = {
 		});
 	},
 	postCollectionRemove: function(request, response){
-		if(utils.Misc.isNullOrUndefined(request.body.object)) {
+		if(utils.Misc.isNullOrUndefined(request.body.query) && utils.Misc.isNullOrUndefined(request.body.object) && utils.Misc.isEmptyObject(request.query)) {
 			var error = new Error(errors['710']);
 			response.end(utils.Misc.createResponse(null, error, 710));
 			return;
 		}
 
-		__dbOp({ db: request.db, collection: request.params.collection, operation: "remove", object: request.body.object }, 
+		__dbOp({ db: request.db, collection: request.params.collection, operation: "remove", query: request.body.query || request.body.object || request.query }, 
 		function(err, docs){
 			response.send(utils.Misc.createResponse(docs, err));
 		});
 	},
 	postCollectionReplace: function(request, response){
-		if(utils.Misc.isNullOrUndefined(request.body.object)) {
+		if(utils.Misc.isNullOrUndefined(request.body.query) && utils.Misc.isNullOrUndefined(request.body.object) && utils.Misc.isEmptyObject(request.query)) {
 			var error = new Error(errors['710']);
 			response.end(utils.Misc.createResponse(null, error, 710));
 			return;
 		}
 
-		__dbOp({ db: request.db, collection: request.params.collection, operation: "update", object: request.body.object, 
+		__dbOp({ db: request.db, collection: request.params.collection, operation: "update", query: request.body.query || request.body.object || request.query, 
 			values: request.body.values, options: { upsert: request.body.upsert || false, multi: request.body.multi || false } }, 
 		function(err, docs){
 			response.send(utils.Misc.createResponse(docs, err));
 		});
 	},
 	postCollectionUpdate: function(request, response){
-		if(utils.Misc.isNullOrUndefined(request.body.object)) {
+		if(utils.Misc.isNullOrUndefined(request.body.query) && utils.Misc.isNullOrUndefined(request.body.object) && utils.Misc.isEmptyObject(request.query)) {
 			var error = new Error(errors['710']);
 			response.end(utils.Misc.createResponse(null, error, 710));
 			return;
 		}
 
-		__dbOp({ db: request.db, collection: request.params.collection, operation: "update", object: request.body.object, 
+		__dbOp({ db: request.db, collection: request.params.collection, operation: "update", query: request.body.query || request.body.object || request.query, 
 			values: { $set: request.body.values }, options: { upsert: request.body.upsert || false, multi: request.body.multi || false } }, 
 		function(err, docs){
 			response.send(utils.Misc.createResponse(docs, err));

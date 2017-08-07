d482b7c992e5972e11c977d0869fbc8f31e853a9
Franklin Chieze <chieze.franklin@gmail.com>
2017-08-07 10:19:53 +0100

    Add events for when db operations fail, add more data to the event body

--- controllers/api-db.js
+++ controllers/api-db.js
@@ -61,7 +61,10 @@ var __dbOp = function(options, callback){
 				else if (options.operation == "insert") {
 					var collection = db.collection(collectionFullname);
 					collection.insert(options.object, function(err, docs){
-						callback(err, docs);
+						var meta = {
+							object: options.object
+						};
+						callback(err, docs, meta);
 						db.close();
 					});
 				}
@@ -89,7 +92,10 @@ var __dbOp = function(options, callback){
 						options.query._id = new mongoose.mongo.ObjectId(options.query._id);
 					}
 					collection.remove(options.query, function(err, result){
-						callback(err, result);
+						var meta = {
+							query: options.query
+						};
+						callback(err, result, meta);
 						db.close();
 					});
 				}
@@ -99,7 +105,12 @@ var __dbOp = function(options, callback){
 						options.query._id = new mongoose.mongo.ObjectId(options.query._id);
 					}
 					collection.update(options.query, options.values, options.options, function(err, doc){
-						callback(err, doc);
+						var meta = {
+							query: options.query,
+							values: options.values,
+							options: options.options
+						};
+						callback(err, doc, meta);
 						db.close();
 					});
 				}
@@ -115,17 +126,28 @@ module.exports = {
 	delete: function(request, response){
 		__dbOp({ db: request.db, operation: "dropdb" }, 
 		function(err, result){
-			utils.Events.fire('app-db-dropped', 
-				{ body: { app: request.db, result: result }, subscribers: [request.db] }, 
-				request.bolt.token, function(eventError, eventResponse) {});
+			if(!utils.Misc.isNullOrUndefined(err)) {
+				utils.Events.fire('app-db-drop-failed', { body: { app: request.db, error: err }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
+			else if(!utils.Misc.isNullOrUndefined(result)) {
+				utils.Events.fire('app-db-dropped', { body: { app: request.db, result: result }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
 			response.send(utils.Misc.createResponse(result, err));
 		});
 	},
 	deleteCollection: function(request, response){
 		__dbOp({ db: request.db, collection: request.params.collection, operation: "drop" }, 
 		function(err, result){
-			utils.Events.fire('app-collection-dropped', { body: { app: request.db, collection: request.params.collection, result: result }, subscribers: [request.db] }, 
-				request.bolt.token, function(eventError, eventResponse) {});
+			if(!utils.Misc.isNullOrUndefined(err)) {
+				utils.Events.fire('app-collection-drop-failed', { body: { app: request.db, collection: request.params.collection, error: err }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
+			else if(!utils.Misc.isNullOrUndefined(result)) {
+				utils.Events.fire('app-collection-dropped', { body: { app: request.db, collection: request.params.collection, result: result }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
 			response.send(utils.Misc.createResponse(result, err));
 		});
 	},
@@ -163,9 +185,15 @@ module.exports = {
 		}
 
 		__dbOp({ db: request.db, collection: request.params.collection, operation: "insert", object: request.body.object }, 
-		function(err, docs){
-			utils.Events.fire('app-collection-inserted', { body: { app: request.db, collection: request.params.collection, result: docs }, subscribers: [request.db] }, 
-				request.bolt.token, function(eventError, eventResponse) {});
+		function(err, docs, meta){
+			if(!utils.Misc.isNullOrUndefined(err)) {
+				utils.Events.fire('app-collection-insert-failed', { body: { app: request.db, collection: request.params.collection, error: err, meta: meta }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
+			else if(!utils.Misc.isNullOrUndefined(docs)) {
+				utils.Events.fire('app-collection-inserted', { body: { app: request.db, collection: request.params.collection, result: docs, meta: meta }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
 			response.send(utils.Misc.createResponse(docs, err));
 		});
 	},
@@ -177,9 +205,15 @@ module.exports = {
 		}
 
 		__dbOp({ db: request.db, collection: request.params.collection, operation: "remove", query: request.body.query || request.body.object || request.query }, 
-		function(err, docs){
-			utils.Events.fire('app-collection-removed', { body: { app: request.db, collection: request.params.collection, result: docs }, subscribers: [request.db] }, 
-				request.bolt.token, function(eventError, eventResponse) {});
+		function(err, docs, meta){
+			if(!utils.Misc.isNullOrUndefined(err)) {
+				utils.Events.fire('app-collection-remove-failed', { body: { app: request.db, collection: request.params.collection, error: err, meta: meta }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
+			else if(!utils.Misc.isNullOrUndefined(docs)) {
+				utils.Events.fire('app-collection-removed', { body: { app: request.db, collection: request.params.collection, result: docs, meta: meta }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
 			response.send(utils.Misc.createResponse(docs, err));
 		});
 	},
@@ -192,9 +226,15 @@ module.exports = {
 
 		__dbOp({ db: request.db, collection: request.params.collection, operation: "update", query: request.body.query || request.body.object || request.query, 
 			values: request.body.values, options: { upsert: request.body.upsert || false, multi: request.body.multi || false } }, 
-		function(err, docs){
-			utils.Events.fire('app-collection-replaced', { body: { app: request.db, collection: request.params.collection, result: docs }, subscribers: [request.db] }, 
-				request.bolt.token, function(eventError, eventResponse) {});
+		function(err, docs, meta){
+			if(!utils.Misc.isNullOrUndefined(err)) {
+				utils.Events.fire('app-collection-replace-failed', { body: { app: request.db, collection: request.params.collection, error: err, meta: meta }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
+			else if(!utils.Misc.isNullOrUndefined(docs)) {
+				utils.Events.fire('app-collection-replaced', { body: { app: request.db, collection: request.params.collection, result: docs, meta: meta }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
 			response.send(utils.Misc.createResponse(docs, err));
 		});
 	},
@@ -207,9 +247,15 @@ module.exports = {
 
 		__dbOp({ db: request.db, collection: request.params.collection, operation: "update", query: request.body.query || request.body.object || request.query, 
 			values: { $set: request.body.values }, options: { upsert: request.body.upsert || false, multi: request.body.multi || false } }, 
-		function(err, docs){
-			utils.Events.fire('app-collection-updated', { body: { app: request.db, collection: request.params.collection, result: docs }, subscribers: [request.db] }, 
-				request.bolt.token, function(eventError, eventResponse) {});
+		function(err, docs, meta){
+			if(!utils.Misc.isNullOrUndefined(err)) {
+				utils.Events.fire('app-collection-update-failed', { body: { app: request.db, collection: request.params.collection, error: err, meta: meta }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
+			else if(!utils.Misc.isNullOrUndefined(docs)) {
+				utils.Events.fire('app-collection-updated', { body: { app: request.db, collection: request.params.collection, result: docs, meta: meta }, subscribers: [request.db] }, 
+					request.bolt.token, function(eventError, eventResponse) {});
+			}
 			response.send(utils.Misc.createResponse(docs, err));
 		});
 	}
